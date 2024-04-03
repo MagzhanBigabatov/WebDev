9d703fcba92d469a2a05d8f5cd202bd958c8a7eb
MagzhanBigabatov <m_bigabatov@kbtu.kz>
2024-04-03 21:38:44 +0300

    new file

---
+++ web dev labs/lab8/shop_back/api/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0)
+    description = models.TextField(default='')
+    count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+            'category_id': self.category.id,
+        }
+
+
+
+
+
